91147412254e95ff07605485e9c6ce4914f6d73b
9181Abhi <138221842+9181Abhi@users.noreply.github.com>
2025-11-11 11:36:30 +0530

    Update login test case start message

--- src/test/java/com/test/classes/TC_LoginDDT.java
+++ src/test/java/com/test/classes/TC_LoginDDT.java
@@ -16,7 +16,7 @@ public class TC_LoginDDT extends Baseclass {
 	
 	{
 		
-		System.out.println("***DDT Login Test case start***");
+		System.out.println("***DDT Login Test case start again***");
 		try
 		{
 			
